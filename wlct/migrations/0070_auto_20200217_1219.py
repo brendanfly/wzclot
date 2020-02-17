2eab728159e41a4a59f9ddf504f4a44f21d354dd
brendanfly <brendanflynn@outlook.com>
2020-02-17 12:50:01 -0800

    Update 0070_auto_20200217_1219.py

--- wlct/migrations/0070_auto_20200217_1219.py
+++ wlct/migrations/0070_auto_20200217_1219.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='player',
             name='discord',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='discord', to='wlct.DiscordUser'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='discord_user', to='wlct.DiscordUser'),
         ),
     ]
