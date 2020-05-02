fc35f10b7d29c43d67362eed1b7e4f8fef65b096
brendanfly <brendanflynn@outlook.com>
2020-05-02 13:11:44 -0700

    Update clotbook.py

--- wlct/clotbook.py
+++ wlct/clotbook.py
@@ -150,7 +150,7 @@ class CLOTBook(models.Model):
             bets = Bet.objects.filter(game=game)
             for bet in bets:
                 if bet.players == winning_players:
-                    bet.winnings = self.calculate_decimal_odds_winnings(bet.odds.decimal_odds, bet.wager)
+                    bet.winnings = self.calculate_decimal_odds_winnings(bet.decimal_odds, bet.wager)
                 else:
                     bet.winnings = 0
                 bet.save()
