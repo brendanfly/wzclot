1dc8f30065d15a7c6ebbe95190b4461765c1a930
brendanfly <brendanflynn@outlook.com>
2020-05-11 12:01:36 -0700

    Fix bug in autoboot/directboot when they do not exist

--- wlct/api.py
+++ wlct/api.py
@@ -108,8 +108,10 @@ class API2:
                             else:
                                 fmt = '{0.days} days {0.hours} hours'
 
-                            ret['directBoot'] = fmt.format(rd(minutes=directbootTimeMinutes))
-                            ret['autoBoot'] = fmt.format(rd(minutes=autobootTimeMinutes))
+                            if directbootTimeMinutes is not 'none':
+                                ret['directBoot'] = fmt.format(rd(minutes=directbootTimeMinutes))
+                            if autobootTimeMinutes is not 'none':
+                                ret['autoBoot'] = fmt.format(rd(minutes=autobootTimeMinutes))
             else:
                 # not good, error, TODO: Log???
                 if 'error' in gameInfo:
