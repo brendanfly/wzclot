d38742b3d218c4e27bd3e27b629db839b7a42ee2
brendanfly <brendanflynn@outlook.com>
2020-09-14 13:24:27 -0700

    Add in better shutdown logic inside the engine

--- wlct/management/commands/engine.py
+++ wlct/management/commands/engine.py
@@ -110,10 +110,12 @@ class Command(BaseCommand):
                 print('Waiting for all jobs to finish and shutting process down...')
                 if self.scheduler is not None and self.scheduler.running:
                     print("Scheduler is running...shutting down")
-                    print("Waiting for outstanding work to finish")
-                    self.scheduler.shutdown(wait=True)
                     print("Removing all jobs")
+                    self.scheduler.print_jobs()
                     self.scheduler.remove_all_jobs()
+                    self.scheduler.print_jobs()
+                    self.scheduler.shutdown()
+                    print("Waiting for outstanding work to finish...sleeping for 30 seconds")
                     sys.exit(0)
 
     def schedule_jobs(self):
